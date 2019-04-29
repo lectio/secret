2aa744327a285a920852f8c1e44fe5e544521fc2
Shahid Shah <shahid@shah.org>
2019-04-28 22:00:10 -0400

    Add comments

--- vault.go
+++ vault.go
@@ -1,19 +1,18 @@
 package secret
 
 import (
+	"fmt"
 	"os"
 	"errors"
 	"net/url"
 )
 
-var ErrInvalidVaultScheme = errors.New("Invalid Value Schema in URL")
-var ErrUnknownVaultScheme = errors.New("Don't know how to handle Vault Scheme")
-var ErrEnvVarNotFound = errors.New("env Vault Scheme's variable not found")
-
+// Vault defines a URL with instructions for how to encrypt/decrypt data
 type Vault struct {
 	url *url.URL
 }
 
+// Parse the given text into a Vault instance
 func Parse(urlText string) (*Vault, error) {
 	// parse url
 	u, err := url.Parse(urlText)
@@ -21,37 +20,39 @@ func Parse(urlText string) (*Vault, error) {
 		return nil, err
 	}
 	if u.Scheme == "" {
-		return nil, ErrInvalidVaultScheme
+		return nil, errors.New("url scheme is empty")
 	}
 	return &Vault{url: u}, nil
 }
 
+// EncryptText encrypts text using instructions given in Vault URL
 func (v Vault) EncryptText(text string) (string, error) {
 	switch v.url.Scheme {
 	case "env":
 		passPhrase, ok := os.LookupEnv(v.url.Hostname())
 		if !ok {
-			return "", ErrEnvVarNotFound
+			return "", fmt.Errorf("%q environment variable %q not set", v.url.String(), v.url.Hostname())
 		}
 		return EncryptText(text, passPhrase)
 	case "passwd":
 		return EncryptText(text, v.url.Hostname())
 	default:
-		return "", ErrUnknownVaultScheme
+		return "", fmt.Errorf("Unable to handle unknown scheme %q in %q", v.url.Scheme, v.url.String())
 	}
 }
 
+// DecryptText decrypts text using instructions given in Vault URL
 func (v Vault) DecryptText(text string) (string, error) {
 	switch v.url.Scheme {
 	case "env":
 		passPhrase, ok := os.LookupEnv(v.url.Hostname())
 		if !ok {
-			return "", ErrEnvVarNotFound
+			return "", fmt.Errorf("%q environment variable %q not set", v.url.String(), v.url.Hostname())
 		}
 		return DecryptText(text, passPhrase)
 	case "passwd":
 		return DecryptText(text, v.url.Hostname())
 	default:
-		return "", ErrUnknownVaultScheme
+		return "", fmt.Errorf("Unable to handle unknown scheme %q in %q", v.url.Scheme, v.url.String())
 	}
 }
