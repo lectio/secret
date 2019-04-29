ebdff8bcf6b4fc51b28273c810e03e6187d65c73
Shahid Shah <shahid@shah.org>
2019-04-28 22:55:15 -0400

    Add factory pattern

--- vault.go
+++ vault.go
@@ -1,63 +1,91 @@
 package secret
 
 import (
+	"sync"
 	"fmt"
 	"os"
-	"errors"
 	"net/url"
 )
 
+var factory sync.Map
+
 // Vault defines a URL with instructions for how to encrypt/decrypt data
-type Vault struct {
-	url *url.URL
+type Vault interface {
+	EncryptText(text string) (string, error)
+	DecryptText(text string) (string, error) 
+}
+
+type envVault struct {
+	envVar string
+}
+
+type passPhraseVault struct {
+	passPhrase string
 }
 
 // Parse the given text into a Vault instance
-func Parse(urlText string) (*Vault, error) {
-	// parse url
+func Parse(urlText string) (Vault, error) {
+	result, ok := factory.Load(urlText)
+	if ok {
+		return result.(Vault), nil
+	}
+
 	u, err := url.Parse(urlText)
 	if err != nil {
 		return nil, err
 	}
 	if u.Scheme == "" {
-		return nil, errors.New("url scheme is empty")
+		return nil, fmt.Errorf("url scheme is empty in secret.Parse(%q)", urlText)
 	}
-	return &Vault{url: u}, nil
+
+	switch u.Scheme {
+	case "env":
+		vault := &envVault{envVar: u.Hostname()}
+		factory.Store(urlText, vault)
+		return vault, nil
+	case "passwd":
+		vault := &passPhraseVault{passPhrase: u.Hostname()}
+		factory.Store(urlText, vault)
+		return vault, nil
+	default:
+		return nil, fmt.Errorf("Unable to handle unknown scheme %q in %q", u.Scheme, u.String())
+	}
 }
 
 // EncryptText encrypts text using instructions given in Vault URL
-func (v Vault) EncryptText(text string) (string, error) {
-	switch v.url.Scheme {
-	case "env":
-		passPhrase, ok := os.LookupEnv(v.url.Hostname())
-		if !ok {
-			return "", fmt.Errorf("%q environment variable %q not set", v.url.String(), v.url.Hostname())
-		}
-		return EncryptText(text, passPhrase)
-	case "passwd":
-		return EncryptText(text, v.url.Hostname())
-	default:
-		return "", fmt.Errorf("Unable to handle unknown scheme %q in %q", v.url.Scheme, v.url.String())
+func (v envVault) EncryptText(text string) (string, error) {
+	passPhrase, ok := os.LookupEnv(v.envVar)
+	if !ok {
+		return "", fmt.Errorf("secret.envVault environment variable %q not set", v.envVar)
 	}
+	return EncryptText(text, passPhrase)
 }
 
 // DecryptText decrypts text using instructions given in Vault URL
-func (v Vault) DecryptText(text string) (string, error) {
-	switch v.url.Scheme {
-	case "env":
-		passPhrase, ok := os.LookupEnv(v.url.Hostname())
-		if !ok {
-			return "", fmt.Errorf("%q environment variable %q not set", v.url.String(), v.url.Hostname())
-		}
-		return DecryptText(text, passPhrase)
-	case "passwd":
-		return DecryptText(text, v.url.Hostname())
-	default:
-		return "", fmt.Errorf("Unable to handle unknown scheme %q in %q", v.url.Scheme, v.url.String())
+func (v envVault) DecryptText(text string) (string, error) {
+	passPhrase, ok := os.LookupEnv(v.envVar)
+	if !ok {
+		return "", fmt.Errorf("secret.envVault environment variable %q not set", v.envVar)
 	}
+	return DecryptText(text, passPhrase)
 }
 
 // String satisfies the stringer interface.
-func (v Vault) String() string {
-	return v.String()
+func (v envVault) String() string {
+	return fmt.Sprintf("env://%s", v.envVar)
+}
+
+// EncryptText encrypts text using instructions given in Vault URL
+func (v passPhraseVault) EncryptText(text string) (string, error) {
+	return EncryptText(text, v.passPhrase)
+}
+
+// DecryptText decrypts text using instructions given in Vault URL
+func (v passPhraseVault) DecryptText(text string) (string, error) {
+	return DecryptText(text, v.passPhrase)
+}
+
+// String satisfies the stringer interface.
+func (v passPhraseVault) String() string {
+	return fmt.Sprintf("passwd://%s", v.passPhrase)
 }
